77ae3c6ce93381f4dd276b9e6b343856e7cae8cf
Hasan Karami Moheb <hasankarami2020313@gmail.com>
2023-10-03 11:11:42 +0330

    changes

---
+++ src/Core/Karami.UseCase/PermissionUseCase/DTOs/HTTPs/ReadAllPaginated/ReadAllPaginatedResponse.cs
@@ -0,0 +1,8 @@
+﻿using Karami.UseCase.Commons.DTOs.HTTPs;
+
+namespace Karami.UseCase.UserUseCase.DTOs.HTTPs.SignIn;
+
+public class ReadAllPaginatedResponse : BaseResponse
+{
+    public ReadAllPaginatedResponseBody Body { get; set; }
+}
---
+++ src/Core/Karami.UseCase/PermissionUseCase/DTOs/HTTPs/ReadAllPaginated/ReadAllPaginatedResponseBody.cs
@@ -0,0 +1,7 @@
+﻿namespace Karami.UseCase.UserUseCase.DTOs.HTTPs.SignIn;
+
+public class ReadAllPaginatedResponseBody
+{
+    public string Token { get; set; }
+    public string SecurityKey { get; set; }
+}
